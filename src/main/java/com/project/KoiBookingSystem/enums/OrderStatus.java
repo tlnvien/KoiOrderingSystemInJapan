35a1b077ce975e949bc21d85952070da920da860
luonghongmy <mylhse184354@fpt.edu.vn>
2024-10-25 15:07:24 +0700

    update OrderStatus

--- src/main/java/com/project/KoiBookingSystem/enums/OrderStatus.java
+++ src/main/java/com/project/KoiBookingSystem/enums/OrderStatus.java
@@ -1,10 +1,11 @@
 package com.project.KoiBookingSystem.enums;
 
 public enum OrderStatus {
-    PROCESSING,
-    CONFIRMED,
-    SHIPPING,
-    DELIVERED,
-    CANCELLED,
-    COMPLETED
+    PROCESSING,  //tạo đơn thành công -> đang xử lí
+    PREPARING,  //farm owner đã xác nhận đơn và đang đóng hàng (farm owner update)
+    SHIPPING,  //farm owner dđã ship (farm owner update)
+    RECEIVED,  //con staff nhận đc ở sân bay vn (con staff update)
+    DELIVERING,  //deli staff đang ship (deli staff update)
+    DELIVERED,  //đã giao cho customer (deli staff update)
+    CANCELLED
 }
